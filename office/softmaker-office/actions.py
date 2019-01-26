13883f4e90bd4755b53266e228ab00ffd1c22363
Christoph Tieben <ctieben@gmx.de>
2019-01-26 13:10:10 +0100

    Update Softmaker Office install script #2

--- office/softmaker-office/actions.py
+++ office/softmaker-office/actions.py
@@ -13,10 +13,6 @@ def setup():
     shelltools.system("pwd")
     shelltools.system("ar xf softmaker-office-2018_942-01_amd64.deb")
     shelltools.system("tar xf data.tar.xz")
-    #Don't install separate menu folder entry:
-    #shelltools.system("rm -r etc")
 
 def install():
     pisitools.insinto("/", "usr")
-    pisitools.insinto("/", "opt")
-    pisitools.insinto("/", "etc")
